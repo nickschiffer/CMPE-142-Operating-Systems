12de17443579aa6f5b1ff732bd638c893c3f4c5d
nickschiffer <nick@schiffer.us>
2018-11-01 13:22:04 -0700

    needs to be fixed

---
+++ PP4.c
@@ -0,0 +1,20 @@
+/* Nickolas Schiffer ID# 012279319, GID#37
+    PP#4 Threads */
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+int main() {
+
+}
+
+void *thread1(){
+
+}
+
+void *thread2(){
+    
+}
